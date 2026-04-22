02d31cebb6acf2457e10d4a39770a464a240e3fa
16lazzy <fankaop@yandex.ru>
2026-04-22 10:16:03 +0300

    create models

---
+++ backend/app/models/product.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
+from ..database import Base
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, index=True)
+    price = Column(Float, nullable=False)
+    description = Column(Text)
+    image_url = Column(String)
+    category_id = Column(Integer, ForeignKey('categories.id'), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    category = relationship('Category', back_populates='products')
+
+    def __repr__(self):
+        return f"<Category(id={self.id}, name={self.name}>"
+        
